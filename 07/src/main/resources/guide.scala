07963899fcc56e9133a33ace929f40292e0653bf
hyoukai <hyou.k.aa@m.titech.ac.jp>
2016-05-30 16:50:41 +0900

    school

--- 07/src/main/resources/guide.scala
+++ 07/src/main/resources/guide.scala
@@ -1,14 +1,27 @@
 case class State(person:Int,monster:Int,boat:Int,shore:Boolean)
-case class Load(person:Int,monster:Int)
+case class Load(person:Int,monster:Int,direction:Boolean)
 abstract class Option[+X]
 case class Some[+X](x:X) extends Option[X]
-case class None extends Option[X]
+case object None extends Option[Nothing]
 
 def makeState:(Int)=>(Boolean)=>(Int)=>State
 =
 (n)=>(shore)=>(boat)=>{State(n,n,boat,shore)}
 
-def next:State=>List[Load]
+def next:State=>List[(Load,State)]
+=
+(sta)=>{
+	sta match{
+		case State(p,m,b,s)=>{def change:(Int)=>(Int)=>List[(Load,State)]
+		=(i)=>(j)=>{
+				if(i+j<= b) (Load(i,j,true),State(p-i,m-j,b,s))::change(i)(j+1)
+				else if(i<= b)	change(i+1)(0)	
+				else Nil
+		}
+		change(0)(0)
+	}
+	}
+}
 
 def isLeagal:State=>Boolean
 =
@@ -24,12 +37,20 @@ def isFinal:State=>Boolean
 		case State(p,m,b,s)=>if(p==0 && m==0) true else false
 	}
 }
-
+/*
 def solve:(Int)=>(Int)=>Option[List[Load]]
 =
 (n)=>(boat)=>
 {
 	def sta = makeState(n)(false)(boat)
 
+	def move:(Option[List[Load]])=>List[Load,State]=>Option[List[Load]]
+		=(lst)=>(lstLS)=>{
+			if(isLeagal(sta1) && isLeagal(sta2)) (sta1,sta2) match{
+				case (State(p1,m1,b1,s1),State(p2,n2,b2,s2))=>
+			}else None
+			}
+		}
 	
 }
+*/
--- 07/src/main/scala/testee02.scala
+++ 07/src/main/scala/testee02.scala
@@ -1,5 +1,22 @@
-package gairon.k11.k02
+package gairon.k11.k03
 object Main{
+def head[X]:(List[X])=>X
+=(lst)=>
+{
+	lst match{
+		case e::rst=>e
+	}
+}
+def end[X]:(List[X])=>List[X]
+=(lstdeal)=>
+{
+	lstdeal match{
+		case Nil=>Nil
+		case e::rst=>rst
+	}
+}
+
+
 def triangular:List[List[Double]]=>List[List[Double]]
 =(lst)=>
 {
@@ -42,16 +59,6 @@ def triangular:List[List[Double]]=>List[List[Double]]
 			}
 		}
 
-	
-	def end:(List[List[Double]])=>List[List[Double]]
-		=(lstdeal)=>
-		{
-			lstdeal match{
-				case Nil=>Nil
-				case e::rst=>rst
-			}
-		}
-
 	def cycle:(List[List[Double]])=>List[List[Double]]
 		=(lstcycle)=>
 		{
@@ -62,4 +69,60 @@ def triangular:List[List[Double]]=>List[List[Double]]
 		}
 	head(lst)::cycle(lst)
 }
+
+
+def reverse[X]:List[X]=>List[X] 
+={ 
+	def revsub:List[X]=>List[X]=>List[X]
+		=(lstL)=>(lstR)=>{
+			lstR match{
+				case Nil=>lstL
+				case e::rst=>revsub(e::lstL)(rst)
+		}
+	}
+	revsub(Nil)
+}
+
+def solve:List[List[Double]]=>List[Double]
+=(lst)=>
+{
+	
+	def oneLineAnswer:(Double)=>(Double)=>List[Double]=>List[Double]=>List[Double]=>List[Double]
+		=(and)=>(n)=>(rans)=>(lstans)=>(lstf)=>
+		{
+			lstf match{
+				case y::Nil=>(y-and)/n::rans
+				case x::xs=>oneLineAnswer(x*head(lstans)+and)(n)(rans)(end(lstans))(xs)
+			}
+		}
+	def domany:List[Double]=>List[List[Double]]=>List[Double]
+		=(lstans)=>(lstf)=>
+		{
+			lstf match{
+				case Nil=>lstans
+				case e::rst=> domany(oneLineAnswer(0)(head(e))(lstans)(lstans)(end(e)))(rst)
+			}
+		}
+	domany(Nil)(reverse(triangular(lst)))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
